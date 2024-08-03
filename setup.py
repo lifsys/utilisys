793ddb14c384b7abfe018c4350cdddbfdc9f8d31
lifsys <johnny@lifsys.com>
2024-08-03 11:19:19 -0500

    update

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
+import io
+with io.open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
